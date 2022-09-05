14a7a8500bd41c701beeed5e3c65da999787dafe
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-09-05 10:36:48 -0400

    finished program

---
+++ fibonacci.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int fib[60];
+	fib[0] = 0;
+	fib[1] = 1;
+
+	for (int i = 2; i < 60; i++)
+	{
+		fib[i] = fib[i-1] + fib[i-2];
+	}
+
+	for (int i = 0; i < 60; i++)
+	{
+		cout << fib[i] << endl;
+	}	
+}
